13dda448d89909b19fbaaf8eb0d45b9c2d3dc839
kimdoyeon <48349608+kimdoyoen@users.noreply.github.com>
2021-10-26 03:29:39 +0900

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    //mongoURI: process.env.REACT_APP_MONGO_URI,
-    mongoURI: "mongodb+srv://doyeon:kdy03130827!@cluster0.llwet.mongodb.net/project?retryWrites=true&w=majority",
-    kakaoAPI: process.env.REACT_APP_KAKAO_API_KEY,
-};
